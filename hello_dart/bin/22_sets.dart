89e0d8303ce20762c11587af2e6688200a4842ef
Murat Bilginer <41648384+MuratBilginerSoft@users.noreply.github.com>
2020-09-30 02:17:59 +0300

    Sets

---
+++ hello_dart/bin/22_sets.dart
@@ -0,0 +1,36 @@
+void main(){
+
+  Set<int> set1 = Set();
+
+  Set<String> set2 = Set();
+
+  set1.add(1);
+  set1.add(2);
+
+  print(set1);
+
+  Set<int> set3 = {1,2,3,4};
+  print(set3);
+
+  var list1 = [1,2,4,5];
+  Set<int> set4 = Set.from(list1);
+
+  print(set4);
+
+  for(var i in set4){
+    print(i);
+  }
+
+  set4.forEach((element) {
+
+    print(element);
+
+  });
+
+  bool a = set4.remove(1);
+  print(a);
+  print(set4);
+
+  set4.
+
+}
